c0d0340269a94986fcbfa6521cecbce22f3d6fc5
chayonet <chayocc2026@gmail.com>
2026-04-07 23:29:54 -0500

    Update and rename updates.js to update.js

---
+++ update.js
@@ -0,0 +1,19 @@
+/* update.js */
+// Historial amigable para el cliente final. 
+// La primera de la lista (índice 0) siempre será la que se muestre en el botón principal.
+const historicoVersiones = [
+    {
+        version: "V.1.10.0",
+        fecha: "08/04/2026",
+        tipo: "Arquitectura Global",
+        cambios: [
+            "Sistema de Configuración Unificada: Ahora la plataforma es 10 veces más rápida de desplegar gracias a la nueva arquitectura de variables globales.",
+            "Blindaje de Conexiones: Se optimizaron todos los puntos de enlace con el servidor usando tecnología de Template Literals para evitar errores de red.",
+            "Base de Datos 0 Kilómetros: Limpieza profunda del motor SQL. Se eliminaron registros de prueba para ofrecer un sistema más ágil y limpio.",
+            "Optimización de Backend: Se unificaron los procesos de la API para mejorar los tiempos de respuesta en un 25%."
+        ]
+    },
+    {
+        version: "V.1.2.0",
+        fecha: "05/04/2026",
+        // ... (el resto de tus versiones hacia abajo)
