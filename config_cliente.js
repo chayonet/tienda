296dd489b35343a83365d772408d6befe5ea495e
chayonet <chayocc2026@gmail.com>
2026-04-07 22:40:07 -0500

    Create config_cliente.js

---
+++ config_cliente.js
@@ -0,0 +1,5 @@
+/* ==========================================
+   ARCHIVO: config.js
+   Variable global para conectar toda la app
+========================================== */
+const API_BASE_URL_CLIENTE = "https://apis-yley.onrender.com";
